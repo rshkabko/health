91a9dde48fde2732b19e4aa9f324273630ed643d
rshkabko <r.shkabko@flamix.email>
2023-08-08 20:29:03 +0300

    :middleware typo

--- src/routes.php
+++ src/routes.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Flamix\Health\Controllers\HealthController;
 
-Route::get('/health/status.{response}', [HealthController::class, 'show'])->middelware(['api', 'throttle:600,1']);
+Route::get('/health/status.{response}', [HealthController::class, 'show'])->middleware(['api', 'throttle:600,1']);
